43ec73e9e4ecd364c2e18e93420c47895ef9bd30
Herrad <herrad.koe@gmail.com>
2015-09-16 22:19:21 +0100

    slowed update

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -15,7 +15,7 @@ function createUpdate(player, outgoingEvents, collisionDetection, draw, controls
 
     setInterval(function () {
         outgoingEvents.locationUpdate(player.serialise())
-    }, 1000 / 15);
+    }, 1000 / 10);
 
     return {
         mainLoop: function (canvas, ctx) {
